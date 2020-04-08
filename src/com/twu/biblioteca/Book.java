d9fc05dbcaa89c7577df75b72951e8cc873f95d4
Eu-Loong Lim <euloong@hotmail.com>
2020-04-08 13:52:05 +1000

    [1.2] Added test and method for showing list of books, integrated into BibliotecaApp

---
+++ src/com/twu/biblioteca/Book.java
@@ -0,0 +1,7 @@
+package com.twu.biblioteca;
+
+public class Book {
+    public static void showList() {
+        System.out.println("1. Clean Code\n" + "2. Don't Make Me Think\n" + "3. Test Driven Development\n");
+    }
+}
