f26dcafbeeaf0091e0019856942071bc3be1a3b2
Eu-Loong Lim <euloong@hotmail.com>
2020-04-08 10:55:10 +1000

    [#1.1] Added out folder to gitignore, changed test filename and test class to follow convention, reduced lines of code

--- test/com/twu/biblioteca/WelcomeMessageTest.java
+++ test/com/twu/biblioteca/WelcomeMessageTest.java
@@ -8,18 +8,16 @@ import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 
-public class WelcomeTest {
+public class WelcomeMessageTest {
 
     @Test
     public void shouldWriteWelcomeMessage() {
         ByteArrayOutputStream output = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(output);
-        System.setOut(ps);
+        System.setOut(new PrintStream(output));
 
-        String result = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
         WelcomeMessage.welcome();
 
-        assertEquals(result + System.getProperty("line.separator"), output.toString());
+        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!" + System.getProperty("line.separator"), output.toString());
     }
 }
 
