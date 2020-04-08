1042735a3fd9159c521135f33b443927647a7832
Eu-Loong Lim <euloong@hotmail.com>
2020-04-08 13:39:57 +1000

    [#1.1]added welcome method call to BibliotecaApp

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,12 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.WelcomeMessage;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        WelcomeMessage.welcome();
+        
     }
 }
